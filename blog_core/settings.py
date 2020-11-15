056bf66aaa8a8473af6d44cafeafe35f2b6bec6e
Sourabh <sourabh@testpress.in>
2020-11-15 12:45:09 +0530

    Add custom domain in allowed hosts and disable debug

--- blog_core/settings.py
+++ blog_core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'uh@@fk@w7xm$5vn#l548=f5zqy16^06&5*7b%s8(7f)!3!pv)n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['ilovedj.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['ilovedj.herokuapp.com','www.ilovedjango3000.com','ilovedjango3000.com','127.0.0.1']
 
 
 # Application definition
